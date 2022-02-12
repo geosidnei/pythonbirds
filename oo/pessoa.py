9607b69105eeb6a777e15b21b233868c10f7dfb1
geosidnei <geosidnei@gmail.com>
2022-02-12 16:06:19 -0300

    Criou classe pessoa.py e enviou ao gitHub. Criou atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome = 'Sidnei', idade = 53):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Raimunda')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'sidnei'
+    print(p.nome)
+    print(p.idade)
